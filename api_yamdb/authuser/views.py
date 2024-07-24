07a454740400fcd4d48e955fa2ac54a8611970c1
ViktorZheltov <153507549+ViktorZheltov@users.noreply.github.com>
2024-07-24 22:35:41 +0300

    3

--- api_yamdb/authuser/views.py
+++ api_yamdb/authuser/views.py
@@ -116,6 +116,7 @@ class SignUpViewSet(GenericViewSet, CreateModelMixin):
             return Response(serializer.data, status=status.HTTP_200_OK)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
+
 class TokenView(APIView):
     serializer_class = TokenSerializer
 
