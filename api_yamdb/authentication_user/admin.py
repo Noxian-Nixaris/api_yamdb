baa118f7766839f93258eb6eb4617aae3475dc11
ViktorZheltov <153507549+ViktorZheltov@users.noreply.github.com>
2024-08-07 12:32:11 +0300

    Третье ревью

--- api_yamdb/authentication_user/admin.py
+++ api_yamdb/authentication_user/admin.py
@@ -1,6 +1,5 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
-
 from .models import User
 
 ADDITIONAL_USER_FIELDS = (
@@ -15,7 +14,8 @@ class UserAdmin(BaseUserAdmin):
     list_display = (
         'username', 'email', 'confirmation_code', 'role', 'bio'
     )
-    list_editable = (
-        'password', 'confirmation_code', 'role', 'is_staff', 'is_active'
-    )
+    list_editable = ('confirmation_code', 'role')
 
+    add_fieldsets = BaseUserAdmin.add_fieldsets + ADDITIONAL_USER_FIELDS
+
+    fieldsets = BaseUserAdmin.fieldsets + ADDITIONAL_USER_FIELDS
