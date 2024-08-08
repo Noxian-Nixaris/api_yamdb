5f04e3803eca6478f09b949e42a65ac87c7d98e8
ViktorZheltov <153507549+ViktorZheltov@users.noreply.github.com>
2024-08-08 06:27:22 +0300

    Четвертое ревью

--- api_yamdb/authentication_user/permissions.py
+++ api_yamdb/authentication_user/permissions.py
@@ -4,17 +4,7 @@ from rest_framework.permissions import BasePermission
 class IsAdminOrSuperuser(BasePermission):
     def has_permission(self, request, view):
         user = request.user
-        if request.path.endswith('/me/') and request.method == 'GET':
-            return user.is_authenticated
-
-        if request.method == 'GET':
-            return user.is_authenticated and user.role == 'admin'
-        return True
-
-
-class NotUserForPatch(BasePermission):
-    def has_object_permission(self, request, view, obj):
-        user = request.user
-        if request.method == 'PATCH' and user.role in ['user', 'moderator']:
-            return False
+        if request.method in ['GET', 'DELETE', 'PATCH']:
+            return user.is_authenticated and (user.role == 'admin'
+                                              or user.is_superuser)
         return True
--- api_yamdb/authentication_user/views.py
+++ api_yamdb/authentication_user/views.py
@@ -10,7 +10,7 @@ from rest_framework.decorators import action
 from rest_framework.exceptions import ValidationError
 
 from .models import User
-from .permissions import IsAdminOrSuperuser, NotUserForPatch
+from .permissions import IsAdminOrSuperuser
 from .serializers import UserSerializer, SignUpSerializer, TokenSerializer
 from .utils import send_confirmation_email
 
@@ -24,11 +24,10 @@ class SignUpViewSet(GenericViewSet, CreateModelMixin):
                                       context={'request': request})
         email = request.data.get('email')
         username = request.data.get('username')
-        if serializer.is_valid():
+        if serializer.is_valid(raise_exception=True):
             user, created = User.objects.get_or_create(email=email,
                                                        username=username)
-            if created:
-                send_confirmation_email(user)
+            send_confirmation_email(user)
             return Response(serializer.data, status=status.HTTP_200_OK)
         raise ValidationError(serializer.errors)
 
@@ -41,7 +40,7 @@ class TokenView(APIView):
         serializer = TokenSerializer(data=request.data,
                                      context={'request': request})
         username = request.data.get('username')
-        if serializer.is_valid():
+        if serializer.is_valid(raise_exception=True):
             user = get_object_or_404(User, username=username)
             refresh = RefreshToken.for_user(user)
             token = {'refresh': str(refresh),
@@ -53,28 +52,20 @@ class TokenView(APIView):
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
-    permission_classes = [IsAuthenticated, IsAdminOrSuperuser, NotUserForPatch]
+    permission_classes = [IsAdminOrSuperuser]
     filter_backends = [filters.SearchFilter]
     search_fields = ['username']
     lookup_field = 'username'
     http_method_names = ['get', 'post', 'patch', 'delete']
 
-    @action(detail=False, methods=['get', 'patch'], url_path='me')
+    @action(detail=False, methods=['get', 'patch'], url_path='me',
+            permission_classes=[IsAuthenticated])
     def update_me(self, request, *args, **kwargs):
         """Обработка GET и PATCH запросов для текущего пользователя."""
         user = request.user
-        data = request.data.copy()
-        data.pop('role', None)
-        serializer = UserSerializer(user, data=data, partial=True,
+        serializer = UserSerializer(user, data=request.data, partial=True,
                                     context={'request': request})
-        if serializer.is_valid():
-            serializer.save()
+        if serializer.is_valid(raise_exception=True):
+            serializer.save(role=self.request.user.role)
             return Response(serializer.data, status=status.HTTP_200_OK)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-    def destroy(self, request, *args, **kwargs):
-        """Удаление пользователя."""
-        serializer = self.get_serializer()
-        user = serializer.validate_destroy(request, *args, **kwargs)
-        user.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
